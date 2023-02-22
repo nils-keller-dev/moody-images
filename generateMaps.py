aab3dcd2c31f742704adbbe928d701e2eaa00d5c
tsomic <keller.nils99@gmail.com>
2023-02-22 12:19:12 +0100

    optimizations

--- generateMaps.py
+++ generateMaps.py
@@ -14,32 +14,30 @@ for index, folder in enumerate(sorted(os.listdir(directory))):
     fullfolder = os.path.join(imagesdir, foldername)
     if os.path.isdir(fullfolder):
         faces = ""
-        for file in sorted(os.listdir(fullfolder)):
+        for file in sorted([f for f in os.listdir(fullfolder) if f.endswith(('.jpg', '.jpeg', '.png', '.bmp'))]):
             filepath = os.path.join(fullfolder, file)
-            if os.path.splitext(filepath)[1].lower() in ('.jpg', '.jpeg', '.png', '.bmp'):
-                im = Image.open(filepath)
-                px = im.load()
+            im = Image.open(filepath)
+            px = im.load()
 
-                data = ""
+            data = ""
 
-                for y in range(16):
-                    for x in range(32):
-                        data += str(int(px[x, y][0] == 0))
+            for y in range(16):
+                for x in range(32):
+                    data += str(int(px[x, y][0] == 0))
 
-                face = ""
-                octets = wrap(data, 8)
-                for i in range(64):
-                    face += "0b" + octets[i] + ", "
+            face = ""
+            octets = wrap(data, 8)
+            for i in range(64):
+                face += "0b" + octets[i] + ", "
 
-                face = face[:len(face) - 2]
-                faces += "    {" + face + "},\n"
-        faces = faces[:len(faces) - 2]
+            face = face[:-2]
+            faces += "    {" + face + "},\n"
+        faces = faces[:-2]
         array += "  {\n" + faces + "\n  },\n"
 
-array = array[:len(array) - 2] + "\n};\n"
+array = array[:-2] + "\n};\n"
 
-f = open(os.path.join(maindir, "../moody-arduino/moody/faces.h"), "w")
-f.write(array)
-f.close()
+with open(os.path.join(maindir, "../moody-arduino/moody/faces.h"), "w") as f:
+    f.write(array)
 
 print("done")
