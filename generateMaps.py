398b5babf91c36e0568094dcc96079fa7560ff34
tsomic <keller.nils99@gmail.com>
2023-02-22 12:08:48 +0100

    skip files in images folder

--- generateMaps.py
+++ generateMaps.py
@@ -12,26 +12,27 @@ array = "static const byte allFaces[][2][64] PROGMEM = {\n"
 for index, folder in enumerate(sorted(os.listdir(directory))):
     foldername = folder.decode("utf-8")
     fullfolder = os.path.join(imagesdir, foldername)
-    faces = ""
-    for file in sorted(os.listdir(fullfolder)):
-        im = Image.open(os.path.join(fullfolder, file))
-        px = im.load()
+    if os.path.isdir(fullfolder):
+        faces = ""
+        for file in sorted(os.listdir(fullfolder)):
+            im = Image.open(os.path.join(fullfolder, file))
+            px = im.load()
 
-        data = ""
+            data = ""
 
-        for y in range(16):
-            for x in range(32):
-                data += str(int(px[x, y][0] == 0))
+            for y in range(16):
+                for x in range(32):
+                    data += str(int(px[x, y][0] == 0))
 
-        face = ""
-        octets = wrap(data, 8)
-        for i in range(64):
-            face += "0b" + octets[i] + ", "
+            face = ""
+            octets = wrap(data, 8)
+            for i in range(64):
+                face += "0b" + octets[i] + ", "
 
-        face = face[:len(face) - 2]
-        faces += "    {" + face + "},\n"
-    faces = faces[:len(faces) - 2]
-    array += "  {\n" + faces + "\n  },\n"
+            face = face[:len(face) - 2]
+            faces += "    {" + face + "},\n"
+        faces = faces[:len(faces) - 2]
+        array += "  {\n" + faces + "\n  },\n"
 
 array = array[:len(array) - 2] + "\n};\n"
 
